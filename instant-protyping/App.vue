e92fa18cda0c93e6233c16fe2bc1e9c3a34fcd1f
Sirwan Afifi <sir1afifi@gmail.com>
2019-10-17 14:00:00 +0100

    Add list

--- instant-protyping/App.vue
+++ instant-protyping/App.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Hello</h1>
+    <ul>
+        <li v-for="name in names" v-bind:key="name">{{ name }}</li>
+    </ul>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            names: ["Sirwan", "Kaywan", "Sana"]
+        }
+    }
+}
+</script>
