1b71017ed2b8e11c807311ab17b72a53e48a30ac
Sirwan Afifi <sir1afifi@gmail.com>
2019-10-18 23:47:45 +0330

    Add avatar

--- instant-protyping/App.vue
+++ instant-protyping/App.vue
@@ -1,6 +1,8 @@
 <template>
     <ul>
-        <li v-for="user in users" v-bind:key="user.id">{{ user.name }}</li>
+        <li v-for="user in users" v-bind:key="user.id">
+            <img :src="'https://ui-avatars.com/api/?name=' + user.name.replace('', '+')" alt="">    {{ user.name }}
+        </li>
     </ul>
 </template>
 
